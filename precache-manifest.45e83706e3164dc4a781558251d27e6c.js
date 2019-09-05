f212d8f0875b8fcf837453873321d2c3c59bcfc4
Nicolas Isnardi <nicolasisnardi@gmail.com>
2019-09-04 22:59:44 -0500

    Updates

---
+++ precache-manifest.45e83706e3164dc4a781558251d27e6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb138aeb1b70bf272f8921a45ed4a0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a3478e778256cc3b7e9",
+    "url": "/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "7c2ab30cdb4f747bd9cd",
+    "url": "/static/js/2.282687f7.chunk.js"
+  },
+  {
+    "revision": "7a3478e778256cc3b7e9",
+    "url": "/static/js/main.9198d0d6.chunk.js"
+  },
+  {
+    "revision": "765801b7eb73936e9c4e",
+    "url": "/static/js/runtime~main.11910b7e.js"
+  }
+]);
